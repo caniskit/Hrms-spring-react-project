94eaa31b6b27334484b2ac69a018b5b944e2296c
caniskit <caniskit@gmail.com>
2021-06-21 22:08:58 +0300

    Inıtialize project layout

---
+++ src/main/java/io/kodlama/hrmsspringboot/HrmsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package io.kodlama.hrmsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmsSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrmsSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/kodlama/hrmsspringboot/HrmsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kodlama.hrmsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmsSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
